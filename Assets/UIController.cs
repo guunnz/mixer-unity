16b01720d8920c139eb427e4cf1795b69e10d898
t <150142740+GRieck2@users.noreply.github.com>
2024-03-06 22:36:05 -0300

    Some Changes

---
+++ Assets/UIController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    // main Menu
+    public TextMeshProUGUI menuNewGame;
+    public TextMeshProUGUI menuContinue;
+    public TextMeshProUGUI menuLeaderboard;
+    public TextMeshProUGUI menuQuit;
+    [Space(10)]
+    //NewGame
+    public TextMeshProUGUI titleTutorial;
+    public TextMeshProUGUI descTutorial;
+    public TextMeshProUGUI titleBack;
+    public TextMeshProUGUI descBack;
+    public TextMeshProUGUI titlePayRun;
+    public TextMeshProUGUI descPayRun;
+    public TextMeshProUGUI titleFreeRun;
+    public TextMeshProUGUI descFreeRun;
+    [Space( 10 )]
+    //OptionsMenu
+    public TextMeshProUGUI optResolution;
+    public TextMeshProUGUI optGraphics;
+    public TextMeshProUGUI optFullScreen;
+    public TextMeshProUGUI optMusic;
+    public TextMeshProUGUI optSFX;
+    public TextMeshProUGUI optBackMenu;
+    
+    
+    
+    
+    
+    //InGameOverlay
+    
+    //OptionsMenu
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    
+    
+}
