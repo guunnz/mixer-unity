4f3acd2e90601e16eb60e3c9c218a8207a5fe433
t <150142740+GRieck2@users.noreply.github.com>
2024-03-08 20:47:28 -0300

    ASD

--- Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/AxieStatCalculator.cs
+++ Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/AxieStatCalculator.cs
@@ -7,17 +7,23 @@ static public class AxieStatCalculator
 {
     static public float GetAttackSpeed(GetAxiesExample.Stats stats)
     {
-        return 10f / (stats.speed * 0.3f);
+        // el 50 implica que alguien con stats (speed + speed * 0,5) 50 va a atacar 1 vez por segundo
+        // El speed + speed * 0,5 es para agrandar el rango de diferencia entre un axie con 61 y uno con 31
+        return 50f / ( stats.speed + stats.speed * 0.5f );
     }
     
     static public float GetHP(GetAxiesExample.Stats stats)
     {
-        return stats.hp * 5f;
+        float minHp = 300f;
+        //el 27 son la stat mas baja de hp que puede tener un axie, se lo resto para unicamente tener en cuenta los stats extra a eso
+        //el 4 es la importancia de estos stats sobrantes, ( si fuera 1, un axie con 61 de hp tendria 300 + (61-27)
+        return minHp + (4f * (stats.hp - 27f));
     }
     
     static public float GetManaPerAttack(GetAxiesExample.Stats stats)
     {
-        return 10;
+        // le resto o sumo un porcentaje de la diferencia del skill con 35 (numero arbitrario) para marcar una diferencia entre los axies con mucho skill
+        return 10 + ( stats.skill - 35f ) * 0.2f;
     }
 
 
@@ -28,11 +34,32 @@ static public class AxieStatCalculator
 
     static public float GetMaxMana(GetAxiesExample.Stats stats)
     {
-        return 100;
+        return 100f;
+    }
+
+    static public float GetCritChance( GetAxiesExample.Stats stats )
+    {
+        // el 1 es para decir que todos los axies tienen 10% al menos
+        // Esta cuenta devuelve un valor entre 0 y 1 para indicar el porcentaje de chance de critico
+        // Le resto el 27 por que es el stat minimo de moral de un axie y en base a la diferencia la multiplico por 0,5 asi los que mas moral tienen tienen mas chances
+        return ( 10f + ( ( stats.morale - 27f ) * 0.5f ) ) * 0.01f;
+    }
+
+    static public float GetCritDamage( GetAxiesExample.Stats stats )
+    {
+        // el 2 es para indicar que se va a hacer el doble de daño si el axie critea
+        // el 35 es elegido para que axies con menos moral que esa (ejemplo pez puro, tengan criticos que hacen menos daño)
+        return 2f + ( stats.morale - 35 ) * 0.02f;
     }
 
     static public float GetAttackDamage(GetAxiesExample.Stats stats)
     {
-        return (stats.speed + stats.morale) * 0.1f;
+        // esta cuenta devuelve un valor que ronda entre los 10 y 20 para calcular el daño de cada auto ataque
+        // speed afecta en 9% de su valor
+        // moral afecta en 20% de su valor
+        // hp afecta en 30% del valor que supera el hp minimo
+        // skill suma o resta un 25% de la diferencia entre el skill y 31 (un numero arbitrario para marcar la norma de skill de un axie)
+        // Este numero lo multiplico por un coeficiente para reducirlo levemente por las dudas, este coeficiente puede no estar
+        return ( stats.speed * 0.09f + stats.morale * 0.2f + ( stats.hp - 27 ) * 0.3f + ( stats.skill - 31f ) * 0.25f ) * 0.9f;
     }
 }
