40cc2d19d98fea603a0f365ffcd358eaa9e342c7
Tomas Gonzalez <160623682+tomasbraintly@users.noreply.github.com>
2024-07-25 12:08:44 -0300

    Conflicts fix

--- Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/AxieBehavior.cs
+++ Assets/LawlessGames/Tactics Toolkit Pathfinding/Pathfinding/Part 3 - RangeFinding and Path Display/Scripts/AxieBehavior.cs
@@ -232,12 +232,7 @@ public class AxieBehavior : MonoBehaviour
         {
             if (myController.CurrentTarget != null)
             {
-<<<<<<< HEAD
-                if (myController.CurrentTarget.axieSkillEffectManager.IsStenched() && !myController.enemyTeam
-                        .GetCharacters().All(x => x.axieSkillEffectManager.IsStenched()))
-=======
                 if (myController.CurrentTarget.axieSkillEffectManager.IsStenched() && !myController.enemyTeam.GetCharacters().All(x => x.axieSkillEffectManager.IsStenched()))
->>>>>>> main
                 {
                     axieState = AxieState.Idle;
                     myController.CurrentTarget = null;
@@ -262,12 +257,7 @@ public class AxieBehavior : MonoBehaviour
 
             if (myController.CurrentTarget != null)
             {
-<<<<<<< HEAD
-                if (myController.CurrentTarget.axieSkillEffectManager.IsStenched() && !myController.enemyTeam
-                        .GetCharacters().All(x => x.axieSkillEffectManager.IsStenched()))
-=======
                 if (myController.CurrentTarget.axieSkillEffectManager.IsStenched() && !myController.enemyTeam.GetCharacters().All(x => x.axieSkillEffectManager.IsStenched()))
->>>>>>> main
                 {
                     axieState = AxieState.Idle;
                     myController.CurrentTarget = null;
